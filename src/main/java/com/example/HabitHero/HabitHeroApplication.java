92b9d62ad47525e4fc5fcc74469c1db395438f2c
PavelTarasauXau <zefirfricadelkovich@gmail.com>
2025-11-11 20:59:41 +0300

    Initial commit

---
+++ src/main/java/com/example/HabitHero/HabitHeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.HabitHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HabitHero/HabitHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HabitHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
